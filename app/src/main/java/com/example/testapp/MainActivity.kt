bbf3f84384b568dd68e4f0f9ac978cf448d68eb5
Sandi Putra Sundawa <sundawasandi@gmail.com>
2022-06-07 17:02:54 +0700

    initial commit

---
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.testapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.testapp.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.testo.text = "test"
+    }
+}
